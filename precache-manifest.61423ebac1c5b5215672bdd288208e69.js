ada54ba552f2d633f0d17e5b1905e7486c5a6db2
JacyGames <bohdan.danultsiv@gmail.com>
2020-07-24 20:19:45 +0300

    Updates

---
+++ precache-manifest.61423ebac1c5b5215672bdd288208e69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6197da3983f30b0a1a564b60d77dd1a",
+    "url": "/JacyGames/ReduxStore/index.html"
+  },
+  {
+    "revision": "2d5cc5546e89fe16ebfb",
+    "url": "/JacyGames/ReduxStore/static/css/main.d9da0604.chunk.css"
+  },
+  {
+    "revision": "c95ffeb668438d6ff11f",
+    "url": "/JacyGames/ReduxStore/static/js/2.c077e7f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JacyGames/ReduxStore/static/js/2.c077e7f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5cc5546e89fe16ebfb",
+    "url": "/JacyGames/ReduxStore/static/js/main.28381fe2.chunk.js"
+  },
+  {
+    "revision": "953d8895ed666c49d564",
+    "url": "/JacyGames/ReduxStore/static/js/runtime-main.482d21ed.js"
+  }
+]);
